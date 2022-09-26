608d84d0b30212e50d9ae10db6076120604e75d7
SirWesterfield <anastasicrl@gmail.com>
2022-09-26 12:41:07 -0500

    started ItemInfo implementation

---
+++ Project 4/proj4-ItemInfo.cpp
@@ -0,0 +1,94 @@
+/**
+ * file: proj4-ItemInfo.cpp
+ * author: Wesley Anastasi
+ * course: CSI 1440
+ * assignment: Amazon 
+ * due date: 8/29/2022
+ *
+ * date modified: 8/29/2022
+ * - created file
+ * 
+ * template
+ */
+
+#include "proj4-ItemInfo.hpp"
+
+void ItemInfo::setItemId(const char *num)
+{
+    
+}
+    
+void ItemInfo::setDescription(const char *cstr)
+{
+
+}
+
+void ItemInfo::setManCost(const char *num)
+{
+
+}
+
+void ItemInfo::setSellPrice(const char *num)
+{
+
+}
+
+int ItemInfo::getItemId()
+{
+
+}
+
+const char *ItemInfo::getDescription()
+{
+
+}
+
+double ItemInfo::getManCost()
+{
+
+}
+
+double ItemInfo::getSellPrice()
+{
+
+}
+
+void ItemInfo::toAmazonJSON(ostream &out)
+{
+
+}
+
+void ItemInfo::displayItemInfo(ostream &out)
+{
+
+}
+
+double ItemInfo::calcProfit()
+{
+
+}
+
+double stuCstrToDbl(const char *num)
+{
+
+}
+
+void stuDblToCstr(char *cstr, double num)
+{
+
+}
+
+void stuCstrCpy(char *dest, const char *src)
+{
+
+}
+
+int stuCstrLen(const char *src)
+{
+
+}
+
+ostream& printCString(ostream &out, const char *src)
+{
+    
+}
