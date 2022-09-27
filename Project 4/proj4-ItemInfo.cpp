b968fd5ee0c5ba1e4f0da4c91839eb58c37e883f
Wesley Anastasi <anastasicrl@gmail.com>
2022-09-27 00:12:03 -0500

    completed 8 functions for itemlist

--- Project 4/proj4-ItemInfo.cpp
+++ Project 4/proj4-ItemInfo.cpp
@@ -2,60 +2,66 @@
  * file: proj4-ItemInfo.cpp
  * author: Wesley Anastasi
  * course: CSI 1440
- * assignment: Amazon 
- * due date: 8/29/2022
+ * assignment: Project 4 
+ * due date: 10/03/2022
  *
- * date modified: 8/29/2022
+ * date modified: 9/22/2022
  * - created file
  * 
- * template
+ * date modified: 9/26/2022
+ *  - finished implementing most of the functions
+ * 
+ * ItemInfo function implementation
  */
 
 #include "proj4-ItemInfo.hpp"
 
 void ItemInfo::setItemId(const char *num)
 {
-    
+    this->itemId = atoi(num);
 }
     
 void ItemInfo::setDescription(const char *cstr)
 {
-
+    for (int i = 0; i < 40 || cstr[i] != '\0'; i++)
+    {
+        this->description[i] = cstr[i];
+    }
 }
 
 void ItemInfo::setManCost(const char *num)
 {
-
+    this->manCost = atoi(num);
 }
 
 void ItemInfo::setSellPrice(const char *num)
 {
-
+    this->sellPrice = atoi(num);
 }
 
 int ItemInfo::getItemId()
 {
-
+    return this->itemId;
 }
 
 const char *ItemInfo::getDescription()
 {
-
+    return this->description;
 }
 
 double ItemInfo::getManCost()
 {
-
+    return this->manCost;
 }
 
 double ItemInfo::getSellPrice()
 {
-
+    return this->sellPrice;
 }
 
 void ItemInfo::toAmazonJSON(ostream &out)
 {
-
+    
 }
 
 void ItemInfo::displayItemInfo(ostream &out)
---
+++ Project 4/proj4-testmain.cpp
@@ -0,0 +1,23 @@
+/**
+ * file: proj4-testmain.cpp
+ * author: Wesley Anastasi
+ * course: CSI 1440
+ * assignment: Project 4
+ * due date: 10/03/2022
+ *
+ * date modified: 9/26/2022
+ * - created file
+ * 
+ * Testmain
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+
+    return 0;   
+}
