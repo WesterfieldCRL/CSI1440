8f473b4f96264abcf150afddbdd502bd7f68bb2f
SirWesterfield <anastasicrl@gmail.com>
2022-10-12 01:18:23 -0500

    implemented some of BUAthleteList's functions

--- Project 5/proj5-BUAthleteList.cpp
+++ Project 5/proj5-BUAthleteList.cpp
@@ -15,22 +15,63 @@
 
 void BUAthleteList::resizeArray()
 {
-
+    BUAthlete *temp = this->list;
+    this->capacity += 2;
+    this->list = new BUAthlete[this->capacity];
+    for (int i = 0; i < this->size; i++)
+    {
+        this->list[i] = temp[i];
+    }
+    delete temp;
 }
 
 void BUAthleteList::addNCAAAthlete(int id, string name, string LOI, string school)
 {
-
+    BUAthlete temp;
+    temp.setID(id);
+    temp.setLOI(LOI);
+    temp.setName(name);
+    temp.setSchool(school);
+    this->list[this->size] = temp;
+    this->size++;
+    if (this->size > this->capacity)
+    {
+        resizeArray();
+    }
 }
 
 void BUAthleteList::setBUInfo(int index, Position pos, int eval)
 {
-
+    if (index < this->size)
+    {
+        this->list[index].setPosition(pos);
+        this->list[index].setEvaluation(eval);   
+    }
 }
 
 int BUAthleteList::findById(int id)
 {
-   
+    int low = 0;
+    int high = this->size-1;
+    int mid;
+
+    while (low != high)
+    {
+        mid = (low + high)/2;
+        if (id == this->list[mid].getID())
+        {
+            return mid;
+        }
+        else if (id > this->list[mid].getID())
+        {
+            low = mid+1;
+        }
+        else
+        {
+            high = mid-1;
+        }
+    }
+
 }
 
 void BUAthleteList::sortByID()
@@ -50,7 +91,10 @@ void BUAthleteList::sortByEvaluation()
 
 BUAthlete& BUAthleteList::operator[](int ndx)
 {
-
+    if (ndx < this->size)
+    {
+        return this->list[ndx];
+    }
 }
 
 string BUAthleteList::toString()
