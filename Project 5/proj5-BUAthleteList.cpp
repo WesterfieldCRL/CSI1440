b3f1feab38cafddfc9932230f008a2bea619a1ef
Wesley Anastasi <anastasicrl@gmail.com>
2022-10-11 19:57:45 -0500

    moved code to correct file

---
+++ Project 5/proj5-BUAthleteList.cpp
@@ -0,0 +1,59 @@
+/**
+ * file: functionTemplate.cpp
+ * author: Wesley Anastasi
+ * course: CSI 1440
+ * assignment: none
+ * due date: 8/29/2022
+ *
+ * date modified: 8/29/2022
+ * - created file
+ * 
+ * template
+ */
+
+#include "proj5-BUAthleteList.hpp"
+
+void BUAthleteList::resizeArray()
+{
+
+}
+
+void BUAthleteList::addNCAAAthlete(int id, string name, string LOI, string school)
+{
+
+}
+
+void BUAthleteList::setBUInfo(int index, Position pos, int eval)
+{
+
+}
+
+int BUAthleteList::findById(int id)
+{
+   
+}
+
+void BUAthleteList::sortByID()
+{
+
+}
+
+void BUAthleteList::sortByPosition()
+{
+
+}
+
+void BUAthleteList::sortByEvaluation()
+{
+
+}
+
+BUAthlete& BUAthleteList::operator[](int ndx)
+{
+
+}
+
+string BUAthleteList::toString()
+{
+
+}
