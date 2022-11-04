ffabf7f7fc959447778119451a894c98ee36164b
SirWesterfield <anastasicrl@gmail.com>
2022-11-04 14:12:42 -0500

    started documentation for MyVector.h

--- Project 7/proj7-MyVector.h
+++ Project 7/proj7-MyVector.h
@@ -3,10 +3,81 @@
 template <class T>
 class MyVector : public ContainerIfc<T> {
 public:
+
+/** 
+ * MyVector ()
+ * 
+ * creates a MyVector object with size = 0, capacity = 10
+ * 
+ * Parameters: 
+ *      none
+ * 
+ * Output: 
+ *      return:  none
+ *      reference parameters: none 
+ *      stream:  none 
+ */
   MyVector ();
+
+/** 
+ * ~MyVector
+ * 
+ * all memory allocated to the MyVector is deleted
+ * 
+ * Parameters: 
+ *      none
+ * 
+ * Output: 
+ *      return:  none
+ *      reference parameters: none 
+ *      stream:  none 
+ */
   ~MyVector ();
+
+/** 
+ * MyVector
+ * 
+ * creates a MyVector as a copy of the provided MyVector object
+ * 
+ * Parameters: 
+ *      MyVector, object being copied
+ * 
+ * Output: 
+ *      return:  none
+ *      reference parameters: MyVector
+ *      stream:  none 
+ */
   MyVector (const MyVector&);
+
+  /** 
+ * operator=
+ * 
+ * makes an exact copy of the data from the provided MyVector
+ * into this MyVector
+ * 
+ * Parameters: 
+ *      MyVector, object being copied
+ * 
+ * Output: 
+ *      return:  none
+ *      reference parameters: MyVector
+ *      stream:  none 
+ */
   MyVector<T>& operator = (const MyVector&);
+
+  /** 
+ * pushFront
+ * 
+ * 
+ * 
+ * Parameters: 
+ *      none
+ * 
+ * Output: 
+ *      return:  none
+ *      reference parameters: none 
+ *      stream:  none 
+ */
   MyVector<T>& pushFront(T);
   MyVector<T>& pushBack(T);
   MyVector<T>& popFront(T&);
