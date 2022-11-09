5125a0b871f1f5081f7a9ad35611acd9cf96f97c
SirWesterfield <anastasicrl@gmail.com>
2022-11-09 13:04:52 -0600

    finished first file in the project

--- Project 6/proj6-sequentialMain3.cpp
+++ Project 6/proj6-sequentialMain3.cpp
@@ -5,8 +5,88 @@
  * assignment: Project 6
  * due date: 11/14/2022
  *
- * date modified: 11/7/2022
+ * date modified: 11/9/2022
  * - created file
  * 
  * sequential Main
  */
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  string name, choice;
+  int age;
+  bool fileRead = false;
+  ifstream pfile;
+  ofstream opfile;
+
+  //if the user responds yes, opens the input file and 
+  cout << "Would you like to read the data from a file? " << endl;
+  cin >> choice;
+  cin.ignore(3, '\n');
+
+  if (choice == "yes")
+  {
+    fileRead = true;
+    pfile.open("iperson.txt");
+    if( !pfile ) {
+      cerr << "Can't open person.txt for read." << endl;
+      return 1;
+    }
+  }
+
+  //opens the output file
+  opfile.open("operson.txt");
+
+  choice = "yes";
+
+  //reads in data from the input file
+  if( fileRead ) {
+    getline( pfile, name );
+    pfile >> age;
+    pfile.ignore(3, '\n');
+    if( !pfile ) {
+      choice = "no";
+    }
+  }
+
+  //loops untill the user responds no
+  while( choice != "no" ) 
+  {
+    //prompts user for data
+    if( !fileRead ) {
+      cout << "Please enter your name followed by your age." << endl;
+      getline( cin, name );
+      cin >> age;
+      cin.ignore(3, '\n');
+    } 
+
+    //outputs data to file
+    opfile << name << endl;
+    opfile << age << endl;
+
+    //prompts user to continue or exit the loop
+    if( !fileRead ) 
+    {
+      cout << "Do you want to read in another person (yes/no)? " << endl;
+      cin >> choice;
+      cin.ignore(3, '\n');
+    } 
+    else 
+    {
+      getline( pfile, name );
+      pfile >> age;
+      pfile.ignore(3, '\n');
+      if( !pfile ) 
+      {
+        choice = "no";
+      }
+    }
+
+  }
+
+  return 0;
+}
